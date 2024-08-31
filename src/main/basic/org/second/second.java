428a200c728faf7a85194cb095a943e6dc2bacfd
hylester <lhy18hust@gmail.com>
2024-08-31 18:32:12 +0800

    0827:华为笔试

--- src/main/basic/org/second/second.java
+++ src/main/basic/org/second/second.java
@@ -5,15 +5,54 @@ import java.util.Map;
 
 public class second {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-        Map<String, String> map = new HashMap<>();
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            map.put("key" + i, "value" + i);
+        Scanner scanner = new Scanner(System.in);
+
+        // 读取矩阵的行数和列数
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        // 读取每一行的异或和
+        long[] rowXor = new long[n];
+        for (int i = 0; i < n; i++) {
+            rowXor[i] = scanner.nextLong();
+        }
+
+        // 读取每一列的异或和
+        long[] colXor = new long[m];
+        for (int j = 0; j < m; j++) {
+            colXor[j] = scanner.nextLong();
+        }
+
+        // 检查每一行和每一列的异或和是否匹配
+        boolean isValid = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rowXor[i] != colXor[j]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (!isValid) {
+                break;
+            }
+        }
+
+            // 输出结果
+            if (isValid) {
+                System.out.println("YES");
+                // 这里可以使用贪心算法来恢复矩阵
+                // 由于题目没有具体说明如何恢复矩阵，这里简单地输出一个示例矩阵
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        System.out.print(rowXor[i] + " ");
+                    }
+                    System.out.println();
+                }
+            } else {
+                System.out.println("NO");
+            }
+
+            scanner.close();
         }
     }
 }
