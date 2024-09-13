1a7e9ba921b5cf11e3a6db3b98093dad75f7fac1
hylester <lhy18hust@gmail.com>
2024-09-13 23:05:19 +0800

    0913:华为笔试

--- src/main/basic/org/first/first.java
+++ src/main/basic/org/first/first.java
@@ -7,15 +7,35 @@ import java.util.Map;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class first {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-        Map<String, String> map = new HashMap<>();
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            map.put("key" + i, "value" + i);
+        // 每一站的花费
+        int[] nums = {1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4};
+        int n = nums.length;
+        // 地铁最多 k 站
+        int k = 10;
+        // 地铁最多坐的站数
+        int m = 3;
+        // 地铁花费
+        int cost = 2;
+
+        // i 表示 过了 nums[i-1]
+        // j 表示 坐地铁次数， 0， 1， 2， 。。。 k
+        int[][] dp = new int[n+1][k+1];
+        for (int i = 1 ; i <= n; i++) {
+            // 当前站走路花费 nums[i-1]
+            // 地铁是0 只能走过来
+            dp[i][0] = dp[i-1][0] + nums[i-1];
+            for (int j = 1; j <= k; j++) {
+                // dp[i][j]: 从前一站走过来； 从前m站地铁过来
+                dp[i][j] = dp[i-1][j] + nums[i-1];
+                for (int pre = i-1; i - pre >= m && pre >= 0; pre--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[pre][j-1] + cost);
+                }
+            }
         }
+        int res = Integer.MAX_VALUE;
+        for (int j = 0; j <= k; j++) {
+            res = Math.min(dp[n][j], res);
+        }
+        System.out.print(res);
     }
 }
--- src/main/basic/org/second/second.java
+++ src/main/basic/org/second/second.java
@@ -2,18 +2,54 @@ package second;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class second {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-        Map<String, String> map = new HashMap<>();
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            map.put("key" + i, "value" + i);
+    final static long MOD = 1000_000_007;
+    static long[] pow = new long[10003];
+    static {
+        pow[0] = 1;
+        for (int i = 1; i < 10002; i++) {
+            pow[i] = (pow[i-1] * 2) % MOD;
         }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for (int i = 0; i < T; i ++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int[] nums = new int[n];
+            for (int j = 0; j < n; j++) {
+                nums[j] = in.nextInt();
+            }
+            doBiz(n, k, nums);
+        }
+    }
+    private static void doBiz(int n, int k, int[] nums) {
+        // 插入k 次， 表示每次都查最大的
+        // 某部分的和是 m, 插入后
+        // 1： 2m
+        // 2： 4m
+        // k； 2^k m
+        // 最终结果 是 sum + 2^k m - m
+
+        // 1. 计算m : 最大子数组
+        long m = 0;
+        long cur = 0;
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            cur = Math.max(nums[i], cur + nums[i]);
+            m = Math.max(cur, m);
+            // 先求和
+            res = (res + nums[i]) % MOD;
+        }
+        res = res + m * (pow[k]-1);
+        // 负数情况
+        while (res < 0) {
+            res += MOD;
+        }
+        System.out.println(res%MOD);
+        // todo:是否包含特殊情况？ 全负？ 包含
     }
 }
